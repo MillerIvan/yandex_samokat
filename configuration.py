0dd03940b67885a327a58885fbe2b694676dce2f
Ivan Miller <millerivan86@gmail.com>
2023-04-07 14:09:15 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = " https://75fe5a80-748f-480e-a9ff-955dea1d306d.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders/"
+ORDERS_TRACK = "/api/v1/orders/track/"
---
+++ data.py
@@ -0,0 +1,16 @@
+headers = {
+    "Content-Type": "application/json"
+}
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Миллер",
+    "address": "Проспект Ленина 31",
+    "metroStation": "4",
+    "phone": "89999999999",
+    "rentTime": "2",
+    "deliveryDate": "2023-04-06",
+    "comment": "не звонить",
+    "color": [
+        "BLACK"
+    ]
+}
